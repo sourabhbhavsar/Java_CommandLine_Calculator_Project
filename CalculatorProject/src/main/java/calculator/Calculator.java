e2c61a413219bae76a50443f362885e6beae30b6
Sourabh Bhavsar <sourabhbhavsar19@gmail.com>
2018-09-07 14:24:37 -0600

    Update Calculator.java
    Test commit

--- CalculatorProject/src/main/java/calculator/Calculator.java
+++ CalculatorProject/src/main/java/calculator/Calculator.java
@@ -8,7 +8,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Properties;
-
+          
 
 /**
  * The following class will simulate the functioning of a command line calculator
